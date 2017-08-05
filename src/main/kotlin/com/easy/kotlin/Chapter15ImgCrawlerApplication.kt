a0171ba9c9e210db3aced7dda4b94b93efbdddf8
jack <guangjian.cgj@alibaba-inc.com>
2017-08-06 01:00:44 +0800

    百度搜索关键词列表

--- src/main/kotlin/com/easy/kotlin/Chapter15ImgCrawlerApplication.kt
+++ src/main/kotlin/com/easy/kotlin/Chapter15ImgCrawlerApplication.kt
@@ -5,13 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
-
-
+import java.io.File
 
 
 @SpringBootApplication
 @EnableScheduling
-class Chapter15ImgCrawlerApplication: WebMvcConfigurerAdapter() {
+class Chapter15ImgCrawlerApplication : WebMvcConfigurerAdapter() {
     override fun configureContentNegotiation(config: ContentNegotiationConfigurer?) {
         config!!.favorPathExtension(true)
     }
@@ -21,79 +20,5 @@ fun main(args: Array<String>) {
     SpringApplication.run(Chapter15ImgCrawlerApplication::class.java, *args)
 }
 
-val 我图URL文件名 = "我图.url"
-val 百度搜索关键词列表 = arrayOf(
-        "尤物",
-        "性感尤物",
-        "绝色尤物",
-        "绝色尤物极品",
-        "性感诱惑",
-        "绝色性感",
-        "性感90 美女",
-        "激性感美女",
-        "性感10美女",
-        "性感短裤美女",
-        "校花",
-        "最美校花",
-        "性感嫩模",
-        "日本美女",
-        "韩国美女",
-        "亚洲美女",
-        "最美网红",
-        "美女校花",
-        "高中校花",
-        "大学校花",
-        "SNH48黄婷婷",
-        "snh48李艺彤",
-        "snh48",
-        "鞠婧祎",
-        "男人装写真",
-        "郑秀晶",
-        "林允儿",
-        "李知恩",
-        "文艺美女",
-        "美女",
-        "清纯",
-        "清纯性感",
-        "清纯可爱",
-        "萝莉",
-        "超清纯气质美女",
-        "嫩模",
-        "尤物",
-        "裴珠泫",
-        "朴信惠",
-        "金所炫",
-        "李侑菲",
-        "朴河宣",
-        "孙允珠",
-        "朱韵淇",
-        "孙文婷",
-        "李麦文",
-        "陈玟予",
-        "申善雅",
-        "李梦皎",
-        "燕子沁",
-        "马艺文",
-        "欧阳娜娜",
-        "手绘森系小清新插画",
-        "手绘建筑设计",
-        "油画",
-        "国家地理高清壁纸",
-        "国家地理摄影精品",
-        "震撼人心的摄影作品",
-        "世界金奖摄影作品",
-        "设计作品欣赏",
-        "校花",
-        "清纯校花",
-        "90后美女",
-        "00后小美女",
-        "清纯美女"
-)
 
-val 搜狗关键词列表 = arrayOf(
-        "美女",
-        "艺术云图",
-        "壁纸",
-        "家居"
-)
-
+val 百度搜索关键词列表 = File("百度搜索关键词列表.data").readLines()
--- src/main/kotlin/com/easy/kotlin/biz/CrawImagesService.kt
+++ src/main/kotlin/com/easy/kotlin/biz/CrawImagesService.kt
@@ -2,7 +2,6 @@ package com.easy.kotlin.biz
 
 import com.easy.kotlin.chapter11_kotlin_springboot.dao.ImageRepository
 import com.easy.kotlin.chapter11_kotlin_springboot.entity.Image
-import com.easy.kotlin.我图URL文件名
 import com.easy.kotlin.百度搜索关键词列表
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.launch
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Service
 import java.io.File
 import java.net.URL
 import java.nio.charset.Charset
-import java.util.regex.Pattern
 
 /**
  * Created by jack on 2017/7/25.
@@ -118,72 +116,5 @@ class CrawImagesService {
         File(filename).writeBytes(bytes)
     }
 
-    fun getImageUrls(): List<String> {
-        return KFileUtil.getFileLines(我图URL文件名)
-    }
-
-
-    fun writeImgUrls(word: String) {
-        var pn = 30
-        for (i in 1..10) {
-            val imgUrlQuery = "http://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&is=&fp=result&queryWord=${word}&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=&z=3&ic=&word=${word}&s=&se=&tab=&width=0&height=0&face=&istype=&qc=&nc=&fr=&pn=${pn}&rn=30&gsm=b4&1501086462487="
-            println("关键字：${word}")
-            println("imgUrlQuery=${imgUrlQuery}")
-            pn += 30
-
-            val objImgUrlRegex = "\\{\"ObjURL\":(.+),\"FromURL\""
-            val imgUrlJsonString = getUrlContent(imgUrlQuery)
-            println(imgUrlJsonString)
-
-            val p = Pattern.compile(objImgUrlRegex)
-            val lines = imgUrlJsonString.split("\n")
-            lines.forEach {
-                val m = p.matcher(it)
-                while (m.find()) {
-                    try {
-                        val result = m.group()
-                        val startIndex = result.indexOf("{\"ObjURL\":\"") + "{\"ObjURL\":\"".length
-                        val endIndex = result.indexOf("\",\"FromURL\"")
-                        var imgUrl = result.substring(startIndex, endIndex)
-                        imgUrl = imgUrl.replace("\\", "")
-                        if (isOk(imgUrl)) {
-                            KFileUtil.appendFile(imgUrl + "\n", 我图URL文件名)
-                        }
-                    } catch (ex: Exception) {
-                    }
-                }
-            }
-        }
-    }
-
-
-    fun isOk(imgUrl: String): Boolean {
-        println(imgUrl)
-//        val urlConnection = URL(imgUrl).openConnection() as HttpURLConnection
-//        urlConnection.connectTimeout = 1000
-//        val size = urlConnection.contentLength
-//        println("size=${size}")
-//        val responseCode = urlConnection.responseCode
-//        println("responseCode=${responseCode}")
-        val 我图文件 = File(我图URL文件名)
-        if (!我图文件.exists()) {
-            我图文件.createNewFile()
-        }
-        val 美女文件所有行 = KFileUtil.getFileLines(我图URL文件名)
-        return !美女文件所有行.contains(imgUrl) // 重复的 url 不写
-                && imgUrl.endsWith(".jpg")
-                && !imgUrl.contains("baidu.com/")
-                && !imgUrl.contains("126.net")
-                && !imgUrl.contains("pconline.com")
-                && !imgUrl.contains("nipic.com")
-                && !imgUrl.contains("zol.com")
-    }
-
 
 }
-
-//fun main(args: Array<String>) {
-//    val url = "http://pic.sogou.com/pics/channel/getAllRecomPicByTag.jsp?category=美女&tag=全部&start=0&len=20"
-//    val c = CrawImagesService().getUrlContent(url)
-//    println(c)
-//}
